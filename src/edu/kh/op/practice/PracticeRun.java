0cebb5b1a8265f750d606244e1573f1e38e4613d
JungWook87 <kjo6391@gmail.com>
2022-12-07 18:36:54 +0900

    02_operatorHomework

---
+++ src/edu/kh/op/practice/PracticeRun.java
@@ -0,0 +1,11 @@
+package edu.kh.op.practice;
+
+public class PracticeRun {
+	public static void main(String[] args) {
+		OperatorPractice start = new OperatorPractice();
+		
+		//start.practice1();
+		//start.practice2();
+		//start.practice3();
+	}
+}
