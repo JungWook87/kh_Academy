3aba60b5311b91e7966bf1d8385a7be45a895a43
JungWook87 <kjo6391@gmail.com>
2022-12-08 10:15:40 +0900

    operator

--- src/edu/kh/op/practice/OperatorPractice.java
+++ src/edu/kh/op/practice/OperatorPractice.java
@@ -61,5 +61,8 @@ public class OperatorPractice {
 		
 		System.out.println("합계 : " + total);
 		System.out.println("평균 : " + avg);
+		
+		String pass = (kor>=40 && eng>=40 && math>=40 && avg>=60)? "합격" : "불합격";
+		System.out.println(pass);
 	}
 }
