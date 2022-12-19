d1a3631ce4128aa40631eb7b9a3c8b8303a6d647
JungWook87 <kjo6391@gmail.com>
2022-12-19 15:18:25 +0900

    2) 숙제 객체배열 하기~ 검색(지역) 또는 수정은 할 수 있으면 하기

--- src/edu/kh/oarr/model/service/MemberService.java
+++ src/edu/kh/oarr/model/service/MemberService.java
@@ -1,5 +1,125 @@
 package edu.kh.oarr.model.service;
 
-public class MemberService {
+import java.util.Scanner;
 
+import edu.kh.oarr.model.vo.Member;
+
+public class MemberService {
+	private Scanner sc = new Scanner(System.in);
+	
+	// Member 5칸짜리 객체 배열 선언 및 할당
+	private Member[] memberArr = new Member[5];
+	
+	// 현재 로그인한 회원의 정보를 저장할 변수 선언
+	
+	private Member loginMember = null;
+	
+	public MemberService() { // 기본생성자
+		// memberARr 배열 0, 1, 2 인덱스 초기화
+		memberArr[0] = new Member("user01", "111", "홍길동", 30, "서울");
+		memberArr[1] = new Member("user02", "222", "고길동", 25, "경기");
+		memberArr[2] = new Member("user03", "333", "홍길순", 25, "용인");
+		
+	}
+	
+	// 메뉴 출력용 메서드
+	public void displayMenu() {
+		int menuNum = 0;
+		
+		do {
+			System.out.println("\n===== 회원 정보 관리 프로그램 v2 =====");
+			System.out.println("1. 회원 가입");
+			System.out.println("2. 로그인");
+			System.out.println("3. 회원 정보 조회");
+			System.out.println("4. 회원 정보 수정");
+			System.out.println("5. 회원 검색(지역)");
+			System.out.println("0. 프로그램 종료");
+			
+			System.out.println("\n메뉴 입력 >>> ");
+			menuNum = sc.nextInt();
+			sc.nextLine();
+			
+			switch(menuNum) {
+			case 1 : break;
+			case 2 : break;
+			case 3 : break;
+			case 4 : break;
+			case 5 : break;
+			case 0 : System.out.println("\n프로그램을 종료 합니다."); break;
+			default : System.out.println("\n번호를 잘못 입력 하셨습니다.");
+			}
+			
+		} while(menuNum != 0);
+	}
+	
+	public String signUp() {
+		System.out.println("\n===== 회원 가입 =====");
+		
+		// 객체 배열(memberArr)에 가입한 회원 정보를 저장할 예정
+		// -> 새로운 회원 정보를 저장할 공간이 있는지 확인하고
+		//	  빈 공간의 인덱스 번호를 얻어오기 -> 새로운 매서드 작성
+		
+		int index = emptyIndex(); // memberArr 배열에서 비어있는 인덱스를 반환 받음
+		
+		System.out.println("현재 회원 수 : " + index);
+		
+		if(index == -1) { // 비어있는 인덱스가 없을 경우 -> 회원 가입 불가
+			return "회원 가입이 불가능 합니다.(인원 수 초과)";
+		}
+		
+		System.out.print("아이디 : ");
+		String memberId = sc.next();
+		System.out.print("비밀번허 : ");
+		String memberPw = sc.next();
+		System.out.print("비밀번호 확인 : ");
+		String memberPw2 = sc.next();
+		System.out.print("이름 : ");
+		String memberName = sc.next();
+		System.out.print("나이 : ");
+		int memberAge = sc.nextInt();
+		System.out.print("지역 : ");
+		String memberRegion = sc.next();
+		
+		if(memberPw == memberPw2) {
+			// 멤버 객체를 생성해서 할당된 주소를 memberArr의 비어있는 인덱스에 대입
+			memberArr[index] = new Member(memberId, memberPw, memberName, memberAge, memberRegion);
+			
+			return "회원 가입 성공!!";
+			
+		} else {
+			
+			return "회원 가입 실패(비밀번호 불일치)";
+			
+		}
+	}
+	
+	// memberArr의 비어있는 인덱스 번호를 반환하는 메서드
+	// 단, 비어있는 인덱스가 없으면 -1 반환
+	public int emptyIndex() {
+		// 멤버 배열을 0번부터 배열의 끝까지 접근해서 참조하는 값이 null인 경우
+		// 인덱스를 반환하기
+		
+		for(int i = 0; i < memberArr.length; i++) {
+			if(memberArr[i] == null) {
+				return i;
+			} 
+		}
+		// for문을 수행했지만 return이 되지 않은 경우 해당 위치 코드가 수행!
+		// for문에서 return 되지 않았다는 뜻은 배열에 빈칸이 없다는 뜻
+		return -1;
+	}
+	
+	public String login() {
+		
+		// 1) memberaArr 배열 내 요소를 순서대로 접근하여 null이 아닌지 확인
+			// 회원 정보가 있을 경우
+			// 2) 회원정보(memberArr[i])에 아이디, 비밀번호와 
+			//    입력받은 아이디, 비밀번호가 같은지 확인
+				// 3) 로그인 회원 정보 객체(Member)를 참조하는 변수 loginMember에
+				//    현재 접근 중인 memberArr[i] 요소에 저장된 주소를 얕은 복사
+		
+		// 4) 로그인 성공 혹은 실패에 따른 결과값 반환
+		
+		return "";
+	}
 }
--- src/edu/kh/oarr/model/vo/Member.java
+++ src/edu/kh/oarr/model/vo/Member.java
@@ -1,5 +1,80 @@
 package edu.kh.oarr.model.vo;
 
 public class Member {
+	
+	// 필드
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private int meberAge;
+	private String region; // 지역(서울, 경기, 충북, 전남..)
+	
+	// 생성자
+	// 생성자 규칙 
+	// 1. 생성자명과 클래스명이 동일해야 한다
+	// 2. 반환형이 없다
+	public Member() {} // 기본생성자
+	
+	// 매개변수 생성자
+	public Member(String memberId, String memberPw, String memberName, int meberAge, String region) {
+		this.memberId = memberId;
+		this.memberPw = memberPw;
+		this.memberName = memberName;
+		this.meberAge = meberAge;
+		this.region = region;
+	}
 
+	// 메서드
+	// getter / setter
+	
+	/* 다 접근 가능하도록 public
+	 * String 자료형 돌려보내겠다
+	 * 매개변수가 없어서 중복된 이름의 memberId가 없기 때문에 memberId라고만 써도 ok
+	 */
+	public String getMemberId() {
+		return memberId;
+	}
+	
+	/* setter는 말 그대로 셋팅하는 애
+	 * 무서을 셋팅할지 전달받아올 아이가 필요합니다!! => 매개변수
+	 * setter는 보통 반환 값이 없다 => void
+	 */
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+		// return;
+		// 모든 메서드는 종료 시 호출한 곳으로 돌아가기 retrun 구문이 작성되어야 하지만
+		// 단, void일 경우 생략 가능하다. -> 생략 시 컴파일러가 자동 추가 
+	}
+
+	public String getMemberPw() {
+		return memberPw;
+	}
+
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public int getMeberAge() {
+		return meberAge;
+	}
+
+	public void setMeberAge(int meberAge) {
+		this.meberAge = meberAge;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
 }
