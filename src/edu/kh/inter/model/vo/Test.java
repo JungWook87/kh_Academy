ba770502f72faa7ce265624b269904d2f761fb94
JungWook87 <kjo6391@gmail.com>
2022-12-26 12:29:25 +0900

    추상클래스 및 인터페이스

---
+++ src/edu/kh/inter/model/vo/Test.java
@@ -0,0 +1,11 @@
+package edu.kh.inter.model.vo;
+
+public interface Test {
+	
+	public static final int NUM = 0;
+	int NUM2 = 0;
+	
+	public abstract void test();
+	
+	void test2();
+}
---
+++ src/edu/kh/inter/model/vo/WaterLife.java
@@ -0,0 +1,8 @@
+package edu.kh.inter.model.vo;
+
+public interface WaterLife {
+	
+	public static final String LIFE = "";
+	
+	public abstract void water();
+}
